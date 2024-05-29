ad4b844c2ddde9db8b7f9ffeb3b75bb7945de607
Martin Chapman <contact@martinchapman.co.uk>
2024-05-29 11:40:51 +0100

    Update made by Phenoflow

---
+++ python/major-adverse-cardiac-events-mace-cereb---primary.py
@@ -0,0 +1,25 @@
+# Alison K Wright, Evangelos Kontopantelis, Richard Emsley, Ian Buchan, Mamas A Mamas, Naveed Sattar, Darren M Ashcroft, Martin M Rutter, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"G617.00","system":"readv2"},{"code":"G640000","system":"readv2"},{"code":"G61z.00","system":"readv2"},{"code":"G641.11","system":"readv2"},{"code":"Gyu6400","system":"readv2"},{"code":"G64..12","system":"readv2"},{"code":"G618.00","system":"readv2"},{"code":"G613.00","system":"readv2"},{"code":"G664.00","system":"readv2"},{"code":"G671000","system":"readv2"},{"code":"G64z.12","system":"readv2"},{"code":"G61..12","system":"readv2"},{"code":"G61..00","system":"readv2"},{"code":"G661.00","system":"readv2"},{"code":"G64z.00","system":"readv2"},{"code":"G640.00","system":"readv2"},{"code":"G61..11","system":"readv2"},{"code":"G64z200","system":"readv2"},{"code":"Gyu6200","system":"readv2"},{"code":"G662.00","system":"readv2"},{"code":"G602.00","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('major-adverse-cardiac-events-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["major-adverse-cardiac-events-mace-cereb---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["major-adverse-cardiac-events-mace-cereb---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["major-adverse-cardiac-events-mace-cereb---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
